6c39605f7d9dc92ea260d78e6ff7b7f80ce8fec2
rokkkuran <lachlanbtaylor@gmail.com>
2017-01-22 18:19:43 +0000

    markov chain bot based on twitch chat history.

---
+++ twitch/markovechobot.py
@@ -0,0 +1,213 @@
+import socket
+import re
+import csv
+import random
+import numpy as np
+
+from time import sleep
+from datetime import datetime
+
+
+HOST = 'irc.twitch.tv'
+PORT = 6667
+# NICK = 'eponymouse'
+# PASS = 'oauth:bqrru03xz1g5l9ehh6w17zwmuyijzg'  # eponymouse
+
+NICK = 'MarkovEchoBot'
+PASS = 'oauth:l7er3yslp8mthfrwqkrox1livcshyy'  # MarkovEchoBot
+
+RATE = 30  # messages per second
+CHAT_MSG = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+time_format = '%Y-%m-%d %H:%M:%S'
+
+
+class Markov(object):
+    # def __init__(self, text):
+    def __init__(self, words):
+        self.cache = {}
+        # self.words = text.split()
+        self.words = words
+        self.word_size = len(self.words)
+        self.database()
+
+    def triples(self):
+        if len(self.words) < 3:
+            return
+
+        for i in range(len(self.words) - 2):
+            yield (self.words[i], self.words[i+1], self.words[i+2])
+
+    def database(self):
+        for w1, w2, w3 in self.triples():
+            key = (w1, w2)
+            if key in self.cache:
+                self.cache[key].append(w3)
+            else:
+                self.cache[key] = [w3]
+
+    def generate_markov_text(self, size=25):
+        seed = random.randint(0, self.word_size - 3)
+        seed_word, next_word = self.words[seed], self.words[seed + 1]
+        w1, w2 = seed_word, next_word
+        gen_words = []
+        for i in xrange(size):
+            gen_words.append(w1)
+            # choose from the list of possible next states in accordance with
+            # the frequencies at which they occur
+            w1, w2 = w2, random.choice(self.cache[(w1, w2)])
+        gen_words.append(w2)
+        return ' '.join(gen_words)
+
+    def generate_markov_text_close(self, size=25, past_size=100):
+        seed = random.randint(0, self.word_size - 3)
+        seed_word = self.words[-past_size:][seed]
+        next_word = self.words[-past_size:][seed + 1]
+        w1, w2 = seed_word, next_word
+        gen_words = []
+        for i in xrange(size):
+            gen_words.append(w1)
+            w1, w2 = w2, random.choice(self.cache[(w1, w2)])
+        gen_words.append(w2)
+        return ' '.join(gen_words)
+
+
+class Spammer(object):
+    """"""
+    def __init__(self, username, channel):
+        super(Spammer, self).__init__()
+        self.username = username
+        self.channel = channel
+        print username, channel, '\n', '-' * (len(username + channel) + 1)
+
+        self._socket = socket.socket()
+        self._socket.connect((HOST, PORT))
+        self._socket.send("PASS {}\r\n".format(PASS).encode("utf-8"))
+        self._socket.send("NICK {}\r\n".format(self.username).encode("utf-8"))
+        self._socket.send("JOIN {}\r\n".format(self.channel).encode("utf-8"))
+
+    def chat(self, msg):
+        # self._socket.send("PRIVMSG #{} :{}\r\n".format(self.channel, msg))
+        self._socket.send("PRIVMSG {} :{}\r\n".format(self.channel, msg))
+
+    def spam(self, n_words=100, max_len=6):
+        words = []
+        i = 0
+        while True:
+            response = self._socket.recv(1024).decode("utf-8")
+            if response == "PING :tmi.twitch.tv\r\n":
+                # send pong back to prevent timeout
+                self._socket.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+            else:
+                username = re.search(r"\w+", response).group(0)
+                message = CHAT_MSG.sub("", response).strip('\r\n')
+                if len(words) < n_words:
+                    words.extend(message.split())
+                    i += 1
+                else:
+                    for word in message.split():
+                        words.pop(0)
+                        words.append(word)
+                    i += 1
+
+                if 'tmi.twitch.tv' not in message:
+                    print '%s: %s' % (username, message)
+                    try:
+                        if (i % 50 == 0) & (len(words) >= n_words):
+                            spammer = Markov(words)
+                            spam_length = np.random.randint(1, max_len)
+                            spam = spammer.generate_markov_text(spam_length)
+                            a = '*' * (len(spam) + 1)
+                            print '\n\n%s\n%s\n%s\n\n' % (a, spam, a)
+                            self.chat(spam)
+                            i = 0
+                    except UnicodeEncodeError as e:
+                        print '\n\n%s\n\n' % e
+                    except UnicodeEncodeError as e:
+                        print '\n\n%s\n\n' % e
+                    except AttributeError as e:
+                        print '\n\n%s\n\n' % e
+                    except KeyError as e:
+                        print '\n\n%s\n\n' % e
+
+            sleep(1 / float(RATE))
+
+
+class AdmiralSpammer(object):
+    """"""
+    def __init__(self, username, channel):
+        super(AdmiralSpammer, self).__init__()
+        self.username = username
+        self.channel = channel
+        print username, channel, '\n', '-' * (len(username + channel) + 1)
+
+        self._socket = socket.socket()
+        self._socket.connect((HOST, PORT))
+        self._socket.send("PASS {}\r\n".format(PASS).encode("utf-8"))
+        self._socket.send("NICK {}\r\n".format(self.username).encode("utf-8"))
+        self._socket.send("JOIN {}\r\n".format(self.channel).encode("utf-8"))
+
+    def chat(self, msg):
+        # self._socket.send("PRIVMSG #{} :{}\r\n".format(self.channel, msg))
+        self._socket.send("PRIVMSG {} :{}\r\n".format(self.channel, msg))
+
+    def spam(self, n_words=100, max_len=6):
+        words = []
+        i = 0
+        while True:
+            response = self._socket.recv(1024).decode("utf-8")
+            if response == "PING :tmi.twitch.tv\r\n":
+                # send pong back to prevent timeout
+                self._socket.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+            else:
+                username = re.search(r"\w+", response).group(0)
+                message = CHAT_MSG.sub("", response).strip('\r\n')
+                if len(words) < n_words:
+                    words.extend(message.split())
+                    i += 1
+                else:
+                    for word in message.split():
+                        words.pop(0)
+                        words.append(word)
+                    i += 1
+
+                if 'tmi.twitch.tv' not in message:
+                    print '%s: %s' % (username, message)
+                    try:
+                        if (i % 50 == 0) & (len(words) >= n_words):
+                            spammer = Markov(words)
+                            spam_length = np.random.randint(1, max_len)
+                            spam = spammer.generate_markov_text_close(size=spam_length)
+                            a = '*' * (len(spam) + 1)
+                            print '\n\n%s\n%s\n%s\n\n' % (a, spam, a)
+                            self.chat(spam)
+                            i = 0
+                    except UnicodeEncodeError as e:
+                        print '\n\n%s\n\n' % e
+                    except UnicodeEncodeError as e:
+                        print '\n\n%s\n\n' % e
+                    except AttributeError as e:
+                        print '\n\n%s\n\n' % e
+                    except KeyError as e:
+                        print '\n\n%s\n\n' % e
+
+            sleep(1 / float(RATE))
+
+
+def wagamamatv():
+    markov_echo_bot = Spammer(NICK, "#wagamamatv")
+    markov_echo_bot.spam(10000, 30)
+
+
+def admiralbulldog():
+    markov_echo_bot = AdmiralSpammer(NICK, "#admiralbulldog")
+    markov_echo_bot.spam(10000, 5)
+
+admiralbulldog()
+
+# CHAN = "#wagamamatv"
+# CHAN = "#pyrionflax"
+# CHAN = "#sing_sing"
+# CHAN = '#lirik'
+# CHAN = '#purgegamers'
+# CHAN = '#ppd'
+# CHAN = '#admiralbulldog'
