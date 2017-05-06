4f666b022c2706fd39302257bb9da5c16502d5be
rokkkuran <lachlanbtaylor@gmail.com>
2017-05-06 17:27:32 +0100

    Julia based Bee Algorithm implementation.

--- pso/bee.jl
+++ pso/bee.jl
@@ -1,3 +1,4 @@
+import Base.copy
 using Distributions
 
 
@@ -5,6 +6,8 @@ type Bee
   position
   value
 end
+
+Base.copy(m::Bee) = Bee(copy(m.position), copy(m.value))
 
 
 function create_scout_bee(f, search_space, dimensions)
@@ -19,13 +22,14 @@ end
 
 
 get_n_best_bees(bees, n) = sort(bees, by=x->x.value)[1:n]
+get_best_bee(bees) = get_n_best_bees(bees, 1)[1]
 
 
-function recruit_forager_bees(f, bee, n, patch_size, search_space)
+function recruit_forager_bees(f, bee, n, ngh, search_space)
   ss_min, ss_max = search_space
-  forager_bees = [bee]
+  forager_bees = [bee]  # include original bee
   for _ in 1:n
-    Δx = rand(size(bee.position)) * patch_size
+    Δx = rand(size(bee.position)) * ngh
     fbx = rand() < 0.5 ? bee.position + Δx : bee.position - Δx
     #TODO: this will need to be fixed for multidimensional spaces
     if fbx[1] < ss_min[1]
@@ -39,10 +43,10 @@ function recruit_forager_bees(f, bee, n, patch_size, search_space)
 end
 
 
-function best_neighbour_bees(bees, f, n_bees, patch_size, search_space)
+function best_neighbour_bees(bees, f, n_bees, ngh, search_space)
   new_best_local_bees = []
   for bee in bees
-    fb = recruit_forager_bees(f, bee, n_bees, patch_size, search_space)
+    fb = recruit_forager_bees(f, bee, n_bees, ngh, search_space)
     best_local_bee = get_n_best_bees(fb, 1)
     push!(new_best_local_bees, best_local_bee)
   end
@@ -50,50 +54,59 @@ function best_neighbour_bees(bees, f, n_bees, patch_size, search_space)
 end
 
 
+rmse(x::Bee, y::Bee) = sum([i^2 for i in x.position - y.position])^0.5
 
-n = 50  # population size
-m = 15  # best patch size
-e = 3  # elite patch size
-nep = 12  # number of forager bees recruited to elite sites
+
+function bee_algorithm(f, n, m, e, nep, nsp, ngh, max_iter, ngh_scaling_factor, search_space)
+  i = 0
+  error = 77.
+  best_bee = 0
+  while i < max_iter #&& error >= ϵ
+    if i == 0
+      # create initial bee population
+      bees = create_scout_bees(n, f, search_space)
+      best_bee = get_best_bee(bees)
+      println("best_bee = $best_bee")
+    end
+
+    best_bees = get_n_best_bees(bees, m)
+    e_bees = best_bees[1:e]  # elite bees
+    ne_bees = best_bees[e+1:m]  # non-elite bees
+
+    # create forager bees in the neighbourhoods of the best patches and return
+    # the best forager bee for each patch.
+    new_e_bees = best_neighbour_bees(e_bees, f, nep, ngh, search_space)
+    new_ne_bees = best_neighbour_bees(ne_bees, f, nsp, ngh, search_space)
+
+    new_best_bees = vcat(new_e_bees, new_ne_bees)
+    new_best_bee = get_best_bee(new_best_bees)
+    new_scout_bees = create_scout_bees(n-m, f, search_space)
+    bees = vcat(new_best_bees, new_scout_bees)
+
+    error = rmse(new_best_bee, best_bee)
+    best_bee = copy(new_best_bee)
+    println("$i: best_bee=$best_bee | ngh=$ngh; error=$error")
+    ngh *= ngh_scaling_factor
+    i += 1
+  end
+  return best_bee.position
+end
+
+
+n = 50  # population of bees
+m = 15  # number of best patches
+e = 3  # number of elite patches
+nep = 12  # number of forager bees recruited to elite best patches
 nsp = 8  # number of forager bees recruited around the non-elite best patches
-ngh = 1  # neigbourhood size
-max_iter = 10
-epsilon = 0.001  # tolerance
+ngh = 1  # neigbourhood patch size
+max_iter = 100
+ϵ = 0.001  # tolerance
 
-patch_size = 1
-patch_scaling_factor = 0.95
+ngh = 1
+ngh_scaling_factor = 0.95
 
 f(x) = -exp(-(x - 0.8)^2)
-search_space = [0, 1]
+search_space = [-5, 5]
 
-
-i = 0
-while i < max_iter
-  if i == 0
-    # create initial bee population
-    bees = create_scout_bees(n, f, search_space)
-    best_bee = get_n_best_bees(bees, 1)
-    println("best_bee = $best_bee")
-  end
-
-  best_bees = get_n_best_bees(bees, m)
-  e_bees = best_bees[1:e]  # elite bees
-  ne_bees = best_bees[e+1:m]  # non-elite bees
-
-  new_e_bees = best_neighbour_bees(e_bees, f, nep, patch_size, search_space)
-  new_ne_bees = best_neighbour_bees(ne_bees, f, nsp, patch_size, search_space)
-  #
-  # println("new_elite_bees: $(size(new_elite_bees))\n$new_elite_bees")
-  # println("new_non_elite_bees: $(size(new_non_elite_bees))\n$new_non_elite_bees")
-
-  new_best_bees = vcat(new_e_bees, new_ne_bees)
-  # println("best_bee = $(get_n_best_bees(new_best_bees, 1))")
-  new_scout_bees = create_scout_bees(n-m, f, search_space)
-  bees = vcat(new_best_bees, new_scout_bees)
-  # println(size(bees))
-
-  println("$i: best_bee=$(get_n_best_bees(new_best_bees, 1)) | patch_size=$patch_size")
-
-  patch_size *= patch_scaling_factor
-  i += 1
-end
+result = bee_algorithm(f, n, m, e, nep, nsp, ngh, max_iter, ngh_scaling_factor, search_space)
+println("\n\nresult=$result")
